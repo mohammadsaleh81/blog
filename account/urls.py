0ce5b78f4b1d587648d16c921fb8faa4f6af8b36
mohammads alehi <mamadreza8138@gmail.com>
2021-03-23 00:51:24 +0430

    'creat-profile'

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import views
 from django.urls import path
-from .views import ArticleList, ArticleCreate, ArticleUpdate, ArticleDelete
+from .views import ArticleList, ArticleCreate, ArticleUpdate, ArticleDelete, Profile
 
 app_name = 'account'
 urlpatterns = [
@@ -21,4 +21,5 @@ urlpatterns += [
     path('article/create/', ArticleCreate.as_view(), name='article_create'),
     path('article/update/<int:pk>', ArticleUpdate.as_view(), name='article_update'),
     path('article/delete/<int:pk>', ArticleDelete.as_view(), name='article_delete'),
+    path('profile/', Profile.as_view(), name='profile'),
 ]
--- account/views.py
+++ account/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
+from account.forms import ProfileForm
 from account.mixins import FieldsMixin, FormvalidMixin, AuthorAccessMixin, SuperUserAccessMixin
+from account.models import User
 from blog.models import Article
 
 # Create your views here.
@@ -31,3 +33,20 @@ class ArticleDelete(SuperUserAccessMixin,DeleteView):
 	model = Article
 	template_name = 'registration/article_confirm_delete.html'
 	success_url = reverse_lazy('account:home')
+
+class Profile(LoginRequiredMixin,UpdateView):
+	model = User
+	template_name = 'registration/profile.html'
+	success_url = reverse_lazy('account:profile')
+	form_class = ProfileForm
+
+	def get_object(self):
+		return User.objects.get(pk= self.request.user.pk)
+
+
+	def get_form_kwargs(self):
+		kwargs = super(Profile, self).get_form_kwargs()
+		kwargs.update({
+			'user': self.request.user
+		})
+		return kwargs
