d7d99c77a606ee2f5612412369592463a6d312c6
mohammads alehi <mamadreza8138@gmail.com>
2021-07-15 23:37:03 +0430

    chanage database

--- config/settings.py
+++ config/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogDB',
+        'USER' : 'postgres',
+        'PASSWORD' : 'reza8138',
+        'HOST' : 'localhost',
+        'PORT' : 5432
     }
 }
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
